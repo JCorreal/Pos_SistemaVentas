7265a4c900d989a927d8b7c178629140fbbc1c14
JCorreal <37590391+JCorreal@users.noreply.github.com>
2021-05-07 16:16:22 -0500

    Delete Pos_SistemaVentas directory

--- Pos_SistemaVentas/DAL/IDao_Proveedor.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-interface IDao_Proveedor {
-    
-    public function obtenerProveedor($buscardato);   
-    public function buscar($datobuscar);   
-    public function grabarProveedor($categoria);  
-    public function eliminarProveedor($datoEliminar);
-}
