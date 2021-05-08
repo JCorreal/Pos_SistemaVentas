f5a3861ad6b2b5c7bd759cd5af9eef9d3b15acd1
JCorreal <37590391+JCorreal@users.noreply.github.com>
2021-05-08 17:25:10 -0500

    Delete Pos_SistemaVentas directory

--- Pos_SistemaVentas/DAL/IDao_Categoria.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-interface IDao_Categoria {
-  
-   public function obtenerCategoria($buscardato);   
-   public function buscar($datobuscar);   
-   public function grabarCategoria($categoria);  
-   public function eliminarCategoria($datoEliminar);
-}
