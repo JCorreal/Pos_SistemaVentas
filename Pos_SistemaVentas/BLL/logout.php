8a085940a72d545f3b49b9b1286172d08e3dd38e
JCorreal <37590391+JCorreal@users.noreply.github.com>
2021-05-10 16:00:40 -0500

    Delete Pos_SistemaVentas directory

--- Pos_SistemaVentas/BLL/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-     session_destroy();
-     unset($_SESSION['User_Name']);
-     header('location: ../Vistas/index.php');
-     exit();
--- Pos_SistemaVentas/DAL/IDao_Venta.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-interface IDao_Venta {   
-   public function grabarVenta($venta);
-   public function buscar();
-   public function hacerPedido($datobuscar);
-   public function reporte($fechainicio, $fechafin);
-}
