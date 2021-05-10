1000727a965d3cbaa3d1e154c5d012f051963fc0
JCorreal <37590391+JCorreal@users.noreply.github.com>
2021-05-10 18:45:30 -0500

    Delete Pos_SistemaVentas directory

--- Pos_SistemaVentas/DAL/IDao_Cliente.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-interface IDao_Cliente {
-    
-    public function obtenerCliente($buscardato);   
-    public function buscar($datobuscar);   
-    public function grabarCliente($categoria);  
-    public function eliminarCliente($datoEliminar);
-}
