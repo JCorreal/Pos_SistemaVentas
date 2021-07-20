bcf6520b6a9bcbf93a7e79261414f900bcf75df7
JCorreal <37590391+JCorreal@users.noreply.github.com>
2021-07-20 18:15:57 -0500

    Update Dao_Usuario.php

--- Pos_SistemaVentas/DAL/Dao_Usuario.php
+++ Pos_SistemaVentas/DAL/Dao_Usuario.php
@@ -8,17 +8,11 @@ class Dao_Usuario implements IDao_Usuario{
     $cn = Conexion::obtenerConexion();
     try 
      {       
-        $rs= $cn->query("CALL SPR_R_Acceso('" . $username . "', '" . $clave . "')");
-        $vecresultado = array(); 
-        while ($fila = $rs->fetch_row()) {
-               array_push($vecresultado, $fila);                
-        }
+        $rs= $cn->query("CALL SPR_R_Acceso('" . $username . "', '" . $clave . "')");        
+        $fila = $rs->fetch_row();
+        $result = $fila[0];  
         mysqli_free_result($rs);
-        mysqli_close($cn);
-        if ($vecresultado != NULL)
-        {
-          $result =  $vecresultado[0][0];
-        }
+        mysqli_close($cn);       
         return $result; 
      }
      catch (Exception $ex)
